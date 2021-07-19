8adef0cd6f2faadd48cc1135c71ff42d3a71792b
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-07-19 14:14:33 +0600

    learned about class and inheritence

--- utility.js
+++ utility.js
@@ -1,3 +1,25 @@
-import Person from "./person";
+class Human {
+  constructor() {
+    this.gender = "male";
+  }
 
-console.log(Person);
+  printMyGender() {
+    console.log(this.gender);
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super(); //used to initialize the parent class
+    //adding a property using constructor
+    this.name = "max";
+  }
+
+  printMyName() {
+    console.log(this.name);
+  }
+}
+
+const person = new Person(); //creating and instance of the class
+person.printMyName();
+person.printMyGender();
